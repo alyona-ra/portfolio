4c61916168caed27d5d8919318b03747df9b13ae
alyona-ra <70214004+alyona-ra@users.noreply.github.com>
2021-02-28 22:28:48 +0300

    Add files via upload

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,6 +2,10 @@ document.querySelectorAll('.right img').forEach((img) => {
   img.style.height = `${document.querySelectorAll('.right')[0].offsetHeight}px`;
 });
 
+document.querySelectorAll('.right img').forEach((el) => {
+  el.addEventListener('click', openModal);
+});
+
 window.onresize = () => {
   document.querySelectorAll('.right img').forEach((img) => {
     img.style.display = 'none';
@@ -14,3 +18,25 @@ window.onresize = () => {
     img.style.display = '';
   });
 };
+
+function openModal(e) {
+  let body = document.querySelector('body');
+  let modal = document.createElement('div');
+  let modalImg = document.createElement('img');
+
+  body.style.overflow = 'hidden';
+  modal.classList.add('modal');
+  modal.style.top = `${window.scrollY}px`;
+  modal.addEventListener('click', closeModal);
+  modalImg.src = `${e.target.getAttribute('src')}`;
+
+  modal.appendChild(modalImg);
+  body.appendChild(modal);
+}
+
+function closeModal(e) {
+  if (e.target.nodeName == 'DIV') {
+    document.querySelector('.modal').remove();
+    document.querySelector('body').style.overflow = '';
+  }
+}
